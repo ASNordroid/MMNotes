dbe05344163f2ef7424d00249ee9862a2cd00e4e
ASNordroid <n3tstrang3r@yandex.ru>
2019-10-14 03:27:04 +0300

    Add export button in settings

--- app/src/main/java/com/searchingfox/mmnotes/SettingsActivity.kt
+++ app/src/main/java/com/searchingfox/mmnotes/SettingsActivity.kt
@@ -2,10 +2,17 @@ package com.searchingfox.mmnotes
 
 import android.os.Bundle
 import android.preference.PreferenceActivity
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity : PreferenceActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        export_button.setOnClickListener {
+            val dbHandler = MyDBHandler(this, null)
+            dbHandler.exportAllNotes()
+        }
     }
 }
